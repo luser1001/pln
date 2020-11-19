3833c412de0d0d0d8f4d62dea6e64d8981360591
luser1001 <prey2099@gmail.com>
2020-11-19 13:51:23 +0100

    cut-up

--- nltk2.py
+++ nltk2.py
@@ -4,7 +4,8 @@ nltk.download()
 from nltk import word_tokenize, sent_tokenize
 from random import sample
 
-text = """ Un analista de seguridad con sede en España demostró nuevas trampas de captura de satélites que podrían permitir navegar de forma anónima con Internet de alta velocidad """
+text = """ Un analista de seguridad con sede en España demostró nuevas trampas de captura de satélites 
+que podrían permitir navegar de forma anónima con Internet de alta velocidad """
 words = nltk.word_tokenize(text)
 
 print(words)
